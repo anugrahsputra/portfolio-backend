e69dcbd5614be986864f49f216cef5d712912518
anugrahsputra <anugrahsputra@gmail.com>
2026-03-13 20:30:51 +0700

    feat: add environment-based .env file support and gitignore

--- config/config.go
+++ config/config.go
@@ -1,16 +1,31 @@
 package config
 
-import "os"
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 type Config struct {
 	Port        string
 	DatabaseURL string
+	Env         string
 }
 
 func Load() *Config {
+	env := getEnv("GO_ENV", "development")
+
+	// Attempt to load .env file based on environment
+	envFile := ".env." + env
+	if err := godotenv.Load(envFile); err != nil {
+		log.Printf("Warning: No %s file found, using system environment variables", envFile)
+	}
+
 	return &Config{
 		Port:        getEnv("PORT", "8080"),
 		DatabaseURL: getEnv("DATABASE_URL", "postgres://postgres:postgres@localhost:5432/postgres?sslmode=disable"),
+		Env:         env,
 	}
 }
 
