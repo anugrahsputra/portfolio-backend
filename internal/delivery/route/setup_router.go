62a82f0526b0e24cffea52fa397b67d60bd67490
anugrahsputra <anugrahsputra@gmail.com>
2026-03-22 15:31:19 +0700

    feat: add web to cors

--- internal/delivery/route/setup_router.go
+++ internal/delivery/route/setup_router.go
@@ -71,7 +71,7 @@ func SetupRouter(db *config.Database) *gin.Engine {
 
 	route := gin.Default()
 	route.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowOrigins:     []string{"http://localhost:3000", "https://www.downormal.dev"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
